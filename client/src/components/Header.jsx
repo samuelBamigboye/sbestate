d9ee1aef539cfcc2741379738281bf8eb9952fd3
Samuel Bamigboye <samdlery65@gmail.com>
2023-10-11 20:12:55 +0100

    modification

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,29 +1,72 @@
-import { FaSearch} from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
-    <header className='bg-slate-300 shadow-md '>
-      <div className='flex flex-wrap p-2 items-center justify-between max-w-6xl mx-auto'>
+    <header className='bg-slate-200 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <img src="./src/assets/sbstatelogoblack.png" alt="SbEstate" className='h-12 w-12' />
+          <img src="./src/assets/sbstatelogoblack.png" alt="sbestate" className='w-8 h-8' />
         </Link>
-      <form className='bg-slate-100 p-2 rounded-lg flex items-center'>
-          <input type="text" placeholder='Search house...' className='bg-transparent focus:outline-none w-26 sm:w-64' />
-          <FaSearch className='h-4 w-4 text-slate-600' />
+        <form
+          onSubmit={handleSubmit}
+          className='bg-slate-100 p-3 rounded-lg flex items-center'
+        >
+          <input
+            type='text'
+            placeholder='Search house...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className='text-slate-600' />
+          </button>
         </form>
-        <ul className='flex gap-3'>
+        <ul className='flex gap-4'>
           <Link to='/'>
-          <li className='hidden sm:inline text-slate-600'>Home</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              Home
+            </li>
           </Link>
           <Link to='/about'>
-          <li className='hidden sm:inline text-slate-600'>About</li>
+            <li className='hidden sm:inline text-slate-700 hover:underline'>
+              About
+            </li>
           </Link>
-          <Link to='/sign-in'>
-          <li className='text-slate-600'>Sign In</li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img
+                className='rounded-full h-7 w-7 object-cover'
+                src={currentUser.avatar}
+                alt='profile'
+              />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
+            )}
           </Link>
         </ul>
-
       </div>
     </header>
-  )
+  );
 }
