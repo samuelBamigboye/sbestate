cde87dd70b408d580590b2e4c71a92688d5cebc5
Samuel Bamigboye <samdlery65@gmail.com>
2023-10-11 13:13:26 +0100

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
 
 function App() {
-  return (
-    <div>
-      app
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Sign-in" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+    </BrowserRouter>
 }
 
 export default App
---
+++ client/src/pages/Pofile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pofile() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pofile
