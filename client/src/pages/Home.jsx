a7c2c2827abf8c5a718e6c3a103034c680215a8e
Samuel Bamigboye <samdlery65@gmail.com>
2023-10-18 19:48:20 +0100

    modify firebase and home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -39,6 +39,7 @@ export default function Home() {
         const res = await fetch('/api/listing/get?type=sale&limit=4');
         const data = await res.json();
         setSaleListings(data);
+        
       } catch (error) {
         log(error);
       }
